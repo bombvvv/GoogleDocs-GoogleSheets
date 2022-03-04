9f01177e2a97881254ab50c10accf562f192caef
Edoardo11 <edoardomirandola2001@gmail.com>
2022-03-04 10:32:46 +0100

    prova a fare auth

--- test.php
+++ test.php
@@ -7,13 +7,12 @@
   <body>
     <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
     <script>
+      // https://docxsheets.herokuapp.com/
       function onSignIn(googleUser) {
         // Useful data for your client-side scripts:
         var profile = googleUser.getBasicProfile();
         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
         console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
         console.log("Image URL: " + profile.getImageUrl());
         console.log("Email: " + profile.getEmail());
 
@@ -21,6 +20,15 @@
         var id_token = googleUser.getAuthResponse().id_token;
         console.log("ID Token: " + id_token);
       }
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.onload = function() {
+        console.log('Signed in as: ' + xhr.responseText);
+      };
+      xhr.send('idtoken=' + id_token);
+      
     </script>
   </body>
 </html>
